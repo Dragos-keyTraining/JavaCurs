4d3857a6066077ede9be2653a1f789b33c91076e
Dragos Tanta <dragos.tanta@gmail.com>
2024-03-13 20:40:42 +0200

    curs 2 intermediar

---
+++ src/curs2/Tester.java
@@ -0,0 +1,38 @@
+package curs2;
+/*
+ * Conventie naming Java:
+ * --numele claselor este UpperCamelCase Ex: ExempluVariabila
+ * --nume metode si var este lowerCamelCase Ex: numeTester 
+ * 
+ * Bad practice in java : 
+ * clasa exemplu_clasa  sau variabila ex: nume_tester
+ * 
+ */
+
+public class Tester {
+	
+	//variabile
+	private String numeTester;//String --valoare default null
+	private int varstaTester;// numeric--valoare default 0
+	
+
+	//metoda
+	public void afiseazaDetaliiTester() {
+		System.out.println( "Numele testerului este: " + numeTester);
+		System.out.println( "Varsta testerului este: " + varstaTester);
+		System.out.println( "Lungimea numelui este: " + numeTester.length());
+	}
+	
+	//cosntructor default
+	//public Tester() {};
+	
+	//Tester tester1 =  new Tester("Monica", 34);
+	public Tester(String nume, int varsta) {
+		numeTester = nume;
+		varstaTester = varsta;
+	};
+	public Tester(String nume) {
+		numeTester = nume;
+
+	};
+}
