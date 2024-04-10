3fa3cd5d197e4086517e13423439253abbcb48c8
Dragos Tanta <dragos.tanta@gmail.com>
2024-04-10 21:30:43 +0300

    curs10 final

--- src/curs10package2/ClasaCopilProtectedModifier.java
+++ src/curs10package2/ClasaCopilProtectedModifier.java
@@ -3,7 +3,7 @@ package curs10package2;
 import curs10package1.ProtectedAccesModifier;
 
 public class ClasaCopilProtectedModifier extends ProtectedAccesModifier{
-
+	// * Sub-clasa sau clasa copil din  alt pachet : yes
 	public static void main(String[] args) {
 
 		ClasaCopilProtectedModifier obj =  new ClasaCopilProtectedModifier();
