6a390d0bfc5e3984a77e59574a1f4d9799b465ec
Dragos Tanta <dragos.tanta@gmail.com>
2024-04-10 20:32:10 +0300

    curs10 la jumatate

---
+++ src/curs10package2/ClasaCopilProtectedModifier.java
@@ -0,0 +1,15 @@
+package curs10package2;
+
+import curs10package1.ProtectedAccesModifier;
+
+public class ClasaCopilProtectedModifier extends ProtectedAccesModifier{
+
+	public static void main(String[] args) {
+
+		ClasaCopilProtectedModifier obj =  new ClasaCopilProtectedModifier();
+		obj.printMesaj();
+		
+		
+	}
+
+}
